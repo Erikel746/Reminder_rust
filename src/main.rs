5fb0bd66cfbeb10768d33ca021e0fcc7ed49a78a
erikel746 <erikel1612@gmail.com>
2024-11-13 21:36:17 +0100

    testing input with calculator

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut input_sign = String::new();
+    let mut input1 = String::new();
+    let mut input2 = String::new();
+
+    println!("Calculator");
+    println!("Input multi, divide, plus or minus");
+
+    while input_sign.trim() != "multi" && input_sign.trim() != "divide" && input_sign.trim() != "plus" && input_sign.trim() != "minus" {
+        input_sign.clear();
+        io::stdin().read_line(&mut input_sign).expect("Failed to read line");
+
+        if input_sign.trim() != "multi" && input_sign.trim() != "divide" && input_sign.trim() != "plus" && input_sign.trim() != "minus" {
+            println!("Invalid sign, try again:");
+        }
+    }
+
+    println!("Enter first number: ");
+    io::stdin().read_line(&mut input1).expect("Failed to read line");
+
+    println!("Enter second number: ");
+    io::stdin().read_line(&mut input2).expect("Failed to read line");
+
+    // Convert inputs from strings to numbers
+    let input1: f64  = input1.trim().parse().expect("Not a number");
+    let input2: f64 = input2.trim().parse().expect("Not a number");
+
+    let result = if input_sign.trim() == "multi" {
+        input1 * input2
+    } else if input_sign.trim() == "divide" {
+        input1 / input2
+    } else if input_sign.trim() == "plus" {
+        input1 + input2
+    } else {
+        input1 - input2
+    };
+
+    println!("The result is: {}", result);
 }
